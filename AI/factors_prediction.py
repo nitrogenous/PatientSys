17571604216ec69646e59d320c57b154428eaffc
Toprak <toprakkoc@outlook.com>
2019-01-20 02:24:57 +0300

    factor script created

---
+++ AI/factors_prediction.py
@@ -0,0 +1,37 @@
+from numpy import exp, array, random, dot
+import matplotlib.pyplot as plt
+
+class NeuralNetwork():
+	
+	def __init__(self):
+		self.weight = 2 * random.random((5,1)) - 1
+
+	def sigmoid(self,x):
+		return 1 / (1+exp(-x))
+
+	def sigmoid_derivative(self,x):
+		return x * (1-x)
+
+	def think(self,inputs):
+		return self.sigmoid(dot(inputs,self.weight))
+
+	def train(self,trainData_inputs,trainData_outputs,epochs):
+		for epoch in range(epochs):
+			predict = self.think(trainData_inputs)
+			bias = trainData_outputs - predict
+			adjustment = dot(trainData_inputs.T, bias * self.sigmoid_derivative(predict))
+			self.weight += adjustment
+
+
+neural = NeuralNetwork()
+
+print neural.weight
+
+trainData_inputs = array([[0,0,0,0,1],[0,0,0,1,0],[0,0,1,0,0],[0,1,0,0,0],[1,0,0,0,0]])
+trainData_outputs = array([[0,1,0,1,0]]).T
+
+neural.train(trainData_inputs,trainData_outputs, 10000)
+
+print neural.weight
+
+print neural.think(array([1,1,0,0,0]))
