d606404f3f159c0c5f9f1907c1236a843dcc3f0a
Toprak <toprakkoc@outlook.com>
2019-01-20 06:05:12 +0300

    updated welcome php for development

---
+++ Back-End/welcome.php
@@ -0,0 +1,15 @@
+<?php
+include 'main.php';
+
+// if(isset($POST)){
+	if('save' == 'save'){//$POST['save']){
+		connectDB();
+		die('savee');
+	}
+	else if('getUsers' == $POST['getUsers']){
+
+	}
+// }
+// else{
+	// die('Error!');
+// }
