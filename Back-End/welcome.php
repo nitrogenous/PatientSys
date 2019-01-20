871d7ec7588e96ae7514a013a6ebc4dc115a6a88
Toprak <toprakkoc@outlook.com>
2019-01-20 10:16:04 +0300

    refactoring for usage

--- Back-End/welcome.php
+++ Back-End/welcome.php
@@ -1,18 +1,28 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 include 'main.php';
 
-// if(isset($POST)){
-	if('save' == 'save'){//$POST['save']){
-		$info = json_encode($POST['info']);
-		$physical = json_encode($POST['physical']);
-		$factors = json_encode($POST['factors']);
+if(isset($_POST)){
+	if('save' == $_POST['save']){
+		$info = array(name => $_POST['name'], surname => $_POST['surname']);
+		print_r($info);
+		$physical = array(gender => $_POST['gender'], age => $_POST['age'],weight => $_POST['weight']);
+		print_r($physical);
+
+		createCSV('physical',$physical);
+		die(predictPatient('physical_prediction'));
+
+		$factors = array(sport => $_POST['sport'], tobacco => $_POST['tobacco'],alcohol => $_POST['alcohol'], familyHistory => $_POST['familyHistory']);
+		print_r($factors);
 		connectDB();
 		die('savee');
 	}
-	else if('getUsers' == $POST['getUsers']){
+	else if('getUsers' == $_POST['getUsers']){
 
 	}
-// }
-// else{
-	// die('Error!');
-// }
+}
+else{
+	die('Error!');
+}
